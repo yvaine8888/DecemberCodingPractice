a5ab977fbd37be6cf7955377c89f8a58a0f89ac3
Yvaine Liew <yvaine8888@gmail.com>
2024-12-07 02:43:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from problem1 import *
+
+print(is_palindrome("Racecar"))
+print(is_palindrome("Hello"))
+print(is_palindrome("Race car!@#$%"))
---
+++ problem1.py
@@ -0,0 +1,31 @@
+'''
+Problem - Create a function to check if the given string is a palindrome
+
+Need to get rid of captilization, punctuation and empty spaces.
+
+function
+
+make it lower
+replace spaces and punctuation with empty from a string with it all
+
+check if the string is equal to it reversed
+if so return true else return false
+'''
+
+def is_palindrome(s):
+    # Making it all lowercase
+    s = s.lower() 
+
+    # All the things to replace
+    replacing = "''!()-[]{} ;:\"\,<>./?@#$%^&*_~''"
+
+    # Replacing it
+    for ele in replacing:
+        s = s.replace(ele, "")
+
+    # Checking and returning if it is a palindrome
+    if s == s[::-1]:
+        return "The string is a palindrome."
+    else:
+        return "The string is not a palindrome."
+    
